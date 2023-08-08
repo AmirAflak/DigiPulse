29d628c3006790f2c7423fd15ab5dad0f7213970
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-08 21:03:50 +0000

    fix: ModelDefinitionException

--- app/models.py
+++ app/models.py
@@ -4,5 +4,5 @@ from cassandra.cqlengine.models import Model
 
 class Product(Model):
     __keyspace__ = "scraper_app"
-    dkp: columns.Text(primary_key=True, required=True)
-    title: columns.Text()
+    dkp = columns.Text(primary_key=True, required=True)
+    title = columns.Text()
