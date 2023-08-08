9714e249792204cea756a9315635203326fc5553
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-08 22:06:32 +0000

    add: scrape entry with uuid

--- app/crud.py
+++ app/crud.py
@@ -1,11 +1,16 @@
 from cassandra.cqlengine.management import sync_table
 from .db import get_session
-from .models import Product
+from .models import Product, ProductScrapeEvent
+import uuid 
 
 session = get_session()
 sync_table(Product)
+sync_table(ProductScrapeEvent)
+
 
 def create_entry(data: dict):
     return Product.create(**data)
 
-
+def create_scrape_entry(data: dict):
+    data['uuid'] = uuid.uuid1()
+    return ProductScrapeEvent.create(**data)
