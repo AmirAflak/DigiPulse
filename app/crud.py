bce701dbf9ec860177e0325d9588749880280388
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-08 21:13:21 +0000

    add: sync_table to commit table into db server

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,11 @@
+from cassandra.cqlengine.management import sync_table
 from .db import get_session
 from .models import Product
 
 session = get_session()
+sync_table(Product)
 
 def create_entry(data: dict):
     return Product.create(**data)
+
 
