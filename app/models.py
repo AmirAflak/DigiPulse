df8cdc0e150b5378120182f8e122339dea078a07
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-08 21:27:14 +0000

    add: price_str column

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
+# data = {
+#     'dkp': '11400857',
+#     'title': 'product 1'
+# }
 
 class Product(Model):
     __keyspace__ = "scraper_app"
     dkp = columns.Text(primary_key=True, required=True)
     title = columns.Text()
+    price_str = columns.Text(default=("-1"))
