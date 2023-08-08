e8477b0e8d8d12d6b6312fee8f179300971d9902
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-08 22:07:28 +0000

    add: ProductScrapeEvent model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,20 @@
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
-# data = {
-#     'dkp': '11400857',
-#     'title': 'product 1'
-# }
+data = {
+    'dkp': '11400857',
+    'title': 'product 1'
+}
 
 class Product(Model):
     __keyspace__ = "scraper_app"
     dkp = columns.Text(primary_key=True, required=True)
     title = columns.Text()
     price_str = columns.Text(default=("-1"))
+    
+class ProductScrapeEvent(Model):
+    __keyspace__ = "scraper_app"
+    uuid = columns.UUID(primary_key=True)
+    dkp = columns.Text(index=True)
+    title = columns.Text()
+    price_str = columns.Text(default=("-100"))
