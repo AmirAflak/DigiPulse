a88119138a71b9eff46260512cf5c71285cf26e9
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-09 21:25:04 +0000

    add: pydantic config.py

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from pydantic import BaseSetting, Field   
+
+class Settings(BaseSetting):
+    db_client_id: str = Field(..., env="ASTRA_CLIENT_ID")
+    db_client_secret: str = Field(..., env="ASTRA_CLIENT_SECRET")
+    
+    class Config:
+        env_file = ".env"
