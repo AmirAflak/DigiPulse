192978e4eb4ea3e0b92d8fe9a43e6fa40e608f04
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-09 22:07:51 +0000

    feat: to get db session

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from cassandra.cqlengine.management import sync_table
 from .config import get_settings
+from .db import get_session
+from .models import Product, ProductScrapeEvent
 
 settings = get_settings()
 
 app = FastAPI()
 
-@app.get("/")
+session = None
 
+@app.on_event("startup")
+def on_startup():
+    global session
+    session = db.get_session()
+    sync_table(Product)
+    sync_table(ProductScrapeEvent)
+
+@app.get("/")
 def read_index():
     return {"hello": "world", "name": settings.name}
+
+
