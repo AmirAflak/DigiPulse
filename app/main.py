f54fba6dffd02bfe4749c0244ed216e63ca2fc71
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-09 21:44:07 +0000

    fix(get_settings): NameError

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from .config import get_settings
+
+settings = get_settings()
 
 app = FastAPI()
 
 @app.get("/")
 
 def read_index():
-    return {"hello": "world"}
+    return {"hello": "world", "name": settings.name}
