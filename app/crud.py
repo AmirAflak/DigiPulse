0668e356a4eaa109d8214ebd7c8df48458086e7f
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-09 22:08:54 +0000

    remove: session acquisition

--- app/crud.py
+++ app/crud.py
@@ -1,12 +1,6 @@
-from cassandra.cqlengine.management import sync_table
-from .db import get_session
 from .models import Product, ProductScrapeEvent
 import uuid 
 import copy
-
-session = get_session()
-sync_table(Product)
-sync_table(ProductScrapeEvent)
 
 
 def create_entry(data: dict):
