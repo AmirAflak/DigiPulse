a2ee56c404e042f7edc2e407fb010b56c8cb3d84
amiraflak <mehrdad.aflakparast@gmail.com>
2023-09-05 16:14:46 +0000

    add: celery on startup connection init

--- app/worker.py
+++ app/worker.py
@@ -1,5 +1,11 @@
 from celery import Celery
+from celery.signals import beat_init, worker_process_init
+from cassandra.cqlengine import connection
+from cassandra.cqlengine.management import sync_table
+from .models import Product, ProductScrapeEvent
 from .config import get_settings
+from .db import get_cluster, get_session
+
 
 celery_app = Celery(__name__)
 settings = get_settings()
@@ -7,6 +13,22 @@ settings = get_settings()
 REDIS_URL = settings.redis_url
 celery_app.conf.broker_url = REDIS_URL
 celery_app.conf.result_backend = REDIS_URL
+
+Product = models.Product
+ProductScrapeEvent = models.ProductScrapeEvent
+
+def celery_on_startup(*args, **kwargs):
+    print("Hello from startup")
+    # cluster = get_cluster()
+    # session = cluster.connect()
+    # connection.register_connection(str(session), session=session)
+    # connection.set_default_connection(str(session))
+    get_session()
+    sync_table(Product)
+    sync_table(ProductScrapeEvent)
+    
+beat_init.connect(celery_on_startup)
+worker_process_init.connect(celery_on_startup)
 
 @celery_app.task
 def random_task(name):
