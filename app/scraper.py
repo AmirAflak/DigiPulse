459557513e7b729d2c62679b03b5f2ae78999703
amiraflak <mehrdad.aflakparast@gmail.com>
2023-09-07 20:23:34 +0330

    fix: chromedriver and browser versioning conflict

--- app/scraper.py
+++ app/scraper.py
@@ -3,13 +3,14 @@ from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service
+
 
 def get_user_agent():
     return UserAgent().random
 
 class Scraper:
     driver: WebDriver = None 
-
     def get_driver(self):
         if self.driver is None:
             user_agent = get_user_agent()
@@ -17,7 +18,16 @@ class Scraper:
             options.add_argument("--no-sandbox")
             options.add_argument("--headless")
             options.add_argument(f"user-agent={user_agent}")
-            driver = webdriver.Chrome(options=options)
+            # driver = webdriver.Chrome(options=options)
+            
+            # Specify the path to your ChromeDriver executable
+            chrome_driver_path = '/home/mehrdad/chromedriver-linux64/chromedriver'
+            
+            # Update the ChromeDriver executable path and make sure it matches
+            # your current Chrome browser version
+            service = Service(chrome_driver_path)
+            driver = webdriver.Chrome(service=service, options=options)
+            
             self.driver = driver
         return self.driver
     
